f64d3f8788353c052007c2adac9126002771b94c
Brian Phillips <bphillips@cpan.org>
2012-09-24 21:52:08 -0500

    Build results of f84d918 (on master)

--- lib/App/Presto/ArgProcessor.pm
+++ lib/App/Presto/ArgProcessor.pm
@@ -78,7 +78,7 @@ sub _expand_param {
 		}
 	} elsif($param =~ m/^FILE($RE{balanced}{-parens => '[]'})($RE{balanced}{-parens => '[]'})?/){
 		my $file = substr($1, 1, -1);
-		my $encoding = $2 ? substr($2, 1, -1) : 'utf8';
+		my $encoding = $2 ? substr($2, 1, -1) : $self->config->get('binmode') || 'utf8';
 		$replacement = read_file( $file, { binmode => ":encoding($encoding)" } );
 	} elsif($param =~ m/^PROMPT($RE{balanced}{-parens => '[]'})($RE{balanced}{-parens => '[]'})?/){
 		my($prompt,$default) = ($1, $2);
